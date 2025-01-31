2a7f921dec8c829ba8aebdce4be8adefe25fd779
keerthirajsivashankar <keerthirajsivashankar@gmail.com>
2025-01-31 08:43:04 +0530

    day 6

--- Easy/uniqueelement.py
+++ Easy/uniqueelement.py
@@ -1,9 +1,11 @@
 from collections import Counter
-s = input("Enter the string : ")
+
+s = input("Enter the string: ")
 count = Counter(s)
-for i,c in enumerate(s):
+
+for i, c in enumerate(s):
     if count[c] == 1:
-        print(f"The unique value within the string is {c} ad the position is {i}")
+        print(f"The unique value within the string is '{c}' at the position {i}")
         break
-    else:
-        print("Thier is no uniques values.")
+else:  # This runs only if the loop completes without finding a unique character
+    print("There are no unique values.")
