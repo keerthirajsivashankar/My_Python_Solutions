6c0abc4deb8af500950ff6a27b312041a4394a44
keerthirajsivashankar <keerthirajsivashankar@gmail.com>
2025-06-12 18:19:41 +0530

    day idk

---
+++ Easy/3423.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maxAdjacentDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        m = abs(nums[0] - nums[-1])
+
+        for i in range(n - 1):
+            m = max(m, abs(nums[i] - nums[i + 1]))
+
+        return m
+
+if _name_ == "_main_":
+    nums = list(map(int, input("Enter the list of numbers separated by spaces: ").split()))
+    sol = Solution()
+    result = sol.maxAdjacentDistance(nums)
+    print("Maximum adjacent distance:", result)
